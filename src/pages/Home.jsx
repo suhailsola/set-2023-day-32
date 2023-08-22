0db38b1fdfa89302515818c1256dac7838bb6a5e
suhailsola <suhailsola@hotmail.com>
2023-08-22 16:38:56 +0800

    styled home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className=" w-screen h-screen bg-pink-800 flex justify-center items-center">
+      <div className="bg-white p-5 rounded-lg flex flex-col justify-center items-center gap-6">
+        <h1 className="text-4xl font-bold text-pink-800">Home</h1>
+        <div className="flex justify-between gap-5">
+          <button className="bg-pink-800 text-white rounded-lg px-4 py-2">
+            <Link to="/login">Login</Link>
+          </button>
+          <button className="bg-pink-800 text-white rounded-lg px-4 py-2">
+            <Link to="/register">Register</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
